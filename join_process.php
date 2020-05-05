fdbb08f4995ddf2a1d8434fefb3eb57248069cc7
kyi7829 <yi7829@naver.com>
2020-05-05 18:35:12 +0900

    회원가입디자인수정

---
+++ join_process.php
@@ -0,0 +1,10 @@
+<?php
+$id = $_POST['id'];
+$pw = $_POST['pw'];
+$nickname = $_POST['nickname'];
+$sex = $_POST['sex'];
+
+$conn = mysqli_connect('127.0.0.1','root','123456','store') or die("fail");
+$sql = "INSERT INTO user (id,pw,nickname,sex) VALUES ('{$id}','{$pw}','{$nickname}','{$sex}')";
+$result = mysqli_query($conn,$sql);
+?>
