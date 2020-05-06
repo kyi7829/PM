dace43a15f70dfff3b49cd14aa987416799e7c2e
kyi7829 <yi7829@naver.com>
2020-05-06 15:20:18 +0900

    디자인수정시작전

---
+++ piece/section.php
@@ -0,0 +1,33 @@
+<section>
+          <div class="best-menu">
+             
+          </div>
+          <div class="menu-select">
+          <button>단품</button><button>조합</button>
+          </div>
+          <div class="menu-search">
+          <input type="radio">FreshFood <input type="radio">Juice/Cofee <input type="radio">Dessert/Icecream 
+          가격대<input type="text"> ~ <input type="text"> <button>검색</button>
+          </div>
+          <div class="food">
+             <div class="foodImg"><img src="img/ramen.jpg" height="150px" width="150px"></div>
+             <div class="foodEX">
+                <div class="foodInfo">
+                   <div class="num" >1</div>
+                   <div class="foodLogo">CU</div>
+                   <div class="foodName"><b>튼튼정식</b></div>
+                </div>
+                <div class="foodExplain">맛과 가성비 , 영양까지 챙긴 식단</div>
+                <div class="tag">웰빙,댕쟝꾹맛,개꿀</div>
+                <div class="start">☆☆☆☆☆</div>
+             </div>
+             <div class="foodPrice">가격은 <br>$4.5</div>
+          </div>
+          <div class="menu-footer">
+             <div>
+             <p>
+             <input class="writing" type="button" value="글쓰기" onclick="location.href='write.php'">
+             </p>
+             </div>
+          </div>
+       </section>
